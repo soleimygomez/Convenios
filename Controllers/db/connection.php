00554dc8d847e8d94b74ba8cf28210d0edd3dee7
edyson10 <edysonfabian@hotmail.com>
2020-12-16 15:14:35 -0500

    Se modifico la conexion

--- Controllers/db/connection.php
+++ Controllers/db/connection.php
@@ -9,7 +9,7 @@ class Db
 	public static function  getConnect(){
 		if (!isset(self::$instance)) {
 			$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-			self::$instance= new PDO('mysql:host=3.83.175.14;dbname=convenios','root','edyson10',$pdo_options);
+			self::$instance= new PDO('mysql:host=3.83.175.14;dbname=convenios','admin','edyson10',$pdo_options);
 		} 
 		return self::$instance;
 	}
--- Controllers/db/parametros.php
+++ Controllers/db/parametros.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_HOST", "3.83.175.14");
 define('DB_DATABASE', 'convenios');
-define('DB_USER', 'root');
+define('DB_USER', 'admin');
 define('DB_PASSWORD', 'edyson10');
 ?>
